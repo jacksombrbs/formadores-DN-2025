1510a8a0f7286bebb07a9f7b188a3f399672f82b
jacksombrbs <jacksom.barbosa@escola.pr.gov.br>
2025-04-16 22:16:21 +0000

    ATT 16/04 - 2

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
-    let tamanhoAtualFonte = 1;
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-    aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0.1
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
     })
 
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-    diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.1
+    const alternaContraste = document.getElementById('alterna-contraste')
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
     })
 
-    const botaoDeAcessibilidade = document.getElementById ('botao-acessibilidade')
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
-    const opcoesDeAcessibilidade = document.getElementById ('opcoes-acessibilidade')
-    
-    botaoDeAcessibilidade.addEventListener('click', function (){
-        botaoDeAcessibilidade.classList.toggle ('rotacao-botao')
-        opcoesDeAcessibilidade.classList.toggle ('apresenta-lista')
     })
 
-    alternaContraste.addEventListener('click', function(){
+    alternaContraste.addEventListener('click', function () {
         document.body.classList.toggle('alto-contraste')
-})
+    })
+
+
 })
